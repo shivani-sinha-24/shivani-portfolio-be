14b60723dda999ec428aee0e232eee1e16685771
ShivaniSinha <shivanisinha24dec@gmail.com>
2023-10-27 10:42:02 +0530

    backend setup for portfolio

---
+++ index.js
@@ -0,0 +1,15 @@
+import  express  from "express";
+import cors from 'cors';
+
+const app = express()
+app.use(express.json());
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req,res)=> res.send("hello world"));
+app.post("/send-email",(req,res)=>{
+    console.log("object 1111111111111111111", req.body.email)
+    res.status(400).send({message:"Email sent successfully"})
+})
+
+app.listen(3009,()=>console.log('server started on port 3009'))
