57cf7c16d4aa150debef3e1f169d944aaa03e03a
ShivaniSinha <shivanisinha24dec@gmail.com>
2023-10-27 10:48:00 +0530

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req,res)=> res.send("hello world"));
 app.post("/send-email",(req,res)=>{
     console.log("object 1111111111111111111", req.body.email)
-    res.status(400).send({message:"Email sent successfully"})
+    res.status(200).send({message:"Email sent successfully"})
 })
 
 app.listen(3009,()=>console.log('server started on port 3009'))
